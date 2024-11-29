9bbb14b74b14eeffb922de0244f2853b1d25d6b8
yash-copods <yash.thakur@copods.co>
2024-11-29 17:03:06 +0530

    type updated for useref Features/AnimatedTitle

--- src/components/Home/Features.tsx
+++ src/components/Home/Features.tsx
@@ -9,13 +9,12 @@ export const BentoTilt = ({
   className: string;
 }) => {
   const [transformStyle, setTransformStyle] = useState("");
-  const itemRef = useRef(null);
+  const itemRef = useRef<HTMLDivElement>(null);
 
   const handleMouseMove = (event: any) => {
     if (!itemRef.current) return;
 
     const { left, top, width, height } =
-      //@ts-ignore
       itemRef.current.getBoundingClientRect();
 
     const relativeX = (event.clientX - left) / width;
@@ -24,7 +23,7 @@ export const BentoTilt = ({
     const tiltX = (relativeY - 0.5) * 5;
     const tiltY = (relativeX - 0.5) * -5;
 
-    const newTransform = `perspective(700px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
+    const newTransform = `perspective(800px) rotateX(${tiltX}deg) rotateY(${tiltY}deg) scale3d(.95, .95, .95)`;
     setTransformStyle(newTransform);
   };
 
