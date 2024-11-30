4fb4825f36c8fe6993285a848300f72a201e1026
yash-copods <yash.thakur@copods.co>
2024-11-30 23:03:13 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import Features from "./components/Home/Features";
 import FloatingImage from "./components/Home/Story";
 import Contact from "./components/genericComponents/Contact";
 import Footer from "./components/genericComponents/Footer";
+import NavBar from "./components/genericComponents/Navbar";
 
 const App: React.FC = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <Hero />
       <About />
       <Features />
